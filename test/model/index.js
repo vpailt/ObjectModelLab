503745e943db69fc933f32b87357bd4a5b4a6e34
Yoann Pigné <yoann.pigne@gmail.com>
2016-10-19 12:03:56 +0200

    test with json data

--- test/model/index.js
+++ test/model/index.js
@@ -2,6 +2,9 @@ import expect from 'expect';
 
 // import { ??? } from '../../src/model';
 
+import  { data }  from './sensors_data'
+console.log(data.length);
+
 describe('Sensor model tests', () => {
   /* TODO: Ecrire ici la suite de tests pourle modèle objet.*/
 });
--- test/model/sensors_data.js
+++ test/model/sensors_data.js
@@ -1,4 +1,4 @@
-[
+export  const  data = [
   {
     "id": 1234,
     "name": "Température Bureau",
